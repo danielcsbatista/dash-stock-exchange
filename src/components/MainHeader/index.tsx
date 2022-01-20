82a0b456195facc40f017dcfe79ac570e9bcebaa
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-01-19 22:18:08 -0300

    feat: add inside of main header the toggle component

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+import Toggle from '../../components/Toggle';
+import { Container, Profile, Welcome, Username } from './styles';
+import Emojis from '../../utils/emojis';
+
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * Emojis.length);
+    return Emojis[index];
+  }, []);
   return (
     <Container>
-      <h1> Main Header</h1>
+      <Toggle />
+      <Profile>
+        <Welcome>Bem vindo, {emoji}</Welcome>
+        <Username>Lucas</Username>
+      </Profile>
     </Container>
   );
 };
