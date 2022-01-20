e77a5cd04b0b43871f7f46617804aae3b4916e89
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-01-19 21:32:33 -0300

    feat: add components for structure of pages

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './styles';
+const MainHeader: React.FC = () => {
+  return (
+    <Container>
+      <h1> Main Header</h1>
+    </Container>
+  );
+};
+export default MainHeader;
