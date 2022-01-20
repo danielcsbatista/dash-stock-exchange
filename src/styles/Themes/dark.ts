8203a6e0f76dd163082f5e051ff7be7eee87bfe7
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-01-19 21:31:19 -0300

    feat: add new styles theme

---
+++ src/styles/Themes/dark.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'dark',
+  colors: {
+    primary: '#1B1F38',
+    secondary: '#252A48',
+    tertiary: '#313862',
+
+    white: '#fff',
+    black: '#000',
+    gray: '#BFBFBF',
+
+    success: '#4E441F0',
+    info: '#F7931B',
+    warning: '#E44C4E',
+  },
+};
---
+++ src/styles/Themes/light.ts
@@ -0,0 +1,16 @@
+export default {
+  title: 'light',
+  colors: {
+    primary: '#DCDCDC',
+    secondary: '#FFF',
+    tertiary: '#F5f5f5',
+
+    white: '#fff',
+    black: '#000',
+    gray: '#BFBFBF',
+
+    success: '#03bb85',
+    info: '#3b5998',
+    warning: '#ff6961',
+  },
+};
