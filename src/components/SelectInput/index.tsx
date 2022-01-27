99af9c475ff503d5158e106128f803d2a55a421b
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-01-26 22:44:06 -0300

    feat: add new component selectInput

---
+++ src/components/SelectInput/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from './styles';
+
+interface ISelectInputProps {
+  options: {
+    value: string | number;
+    label: string | number;
+  }[];
+  onChange?: (value: string | number) => void;
+}
+const SelectInput: React.FC<ISelectInputProps> = ({ options }) => {
+  return (
+    <Container>
+      <select>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </Container>
+  );
+};
+export default SelectInput;
