074ab590c1e71120565f1cdf588b3b3f1f5ab078
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-01-26 22:46:29 -0300

    feat: add components inside of list

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import ContentHeader from '../../components/ContentHeader';
+import SelectInput from '../../components/SelectInput';
+import { Container } from './styles';
 
 const List: React.FC = () => {
-  return <h1>Dashborad</h1>;
+  const options = [
+    { label: 'Ana', value: 'a' },
+    { label: 'Bruno', value: 'b' },
+    { label: 'Carlos', value: 'c' },
+  ];
+  return (
+    <Container>
+      <ContentHeader title='Saídas' lineColor='#E44C4E'>
+        <SelectInput options={options} />
+      </ContentHeader>
+    </Container>
+  );
 };
 export default List;
