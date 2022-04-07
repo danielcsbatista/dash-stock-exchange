4f95b707695d75e36378bc547f5b77ee451541a0
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-04-06 21:50:11 -0300

    feat: add filters in list page

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useLocation } from 'react-router-dom';
 import ContentHeader from '../../components/ContentHeader';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 import SelectInput from '../../components/SelectInput';
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
-  const options = [
-    { label: 'Ana', value: 'a' },
-    { label: 'Bruno', value: 'b' },
-    { label: 'Carlos', value: 'c' },
+  const { pathname } = useLocation();
+
+  const title = useMemo(() => {
+    return pathname.includes('entry-balance')
+      ? { name: 'Entradas', lineColor: '#F7931B' }
+      : { name: 'Saídas', lineColor: '#E44C4E' };
+  }, [pathname]);
+  const months = [
+    { label: 'julho', value: 7 },
+    { label: 'agosto', value: 8 },
+    { label: 'setembro', value: 9 },
   ];
+
+  const years = [
+    { label: 2020, value: 2020 },
+    { label: 2019, value: 2019 },
+    { label: 2018, value: 2018 },
+  ];
+
   return (
     <Container>
-      <ContentHeader title='Saídas' lineColor='#E44C4E'>
-        <SelectInput options={options} />
+      <ContentHeader title={title.name} lineColor={title.lineColor}>
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type='button' className='tag-filter tag-filter-recurrent'>
+          Recorrentes
+        </button>
+
+        <button type='button' className='tag-filter tag-filter-eventual'>
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((card) => (
           <HistoryFinanceCard
-            cardColor='#313862'
+            key={card}
             tagColor='#E44C4E'
             title={`Salário ${card}`}
             subTitle={'28/02/2022'}
