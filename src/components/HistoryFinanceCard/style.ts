68e8ed5b5ac8305da7c1b6dfd5f494af0a392570
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-04-07 23:56:16 -0300

    fix: fix size font of subtitle

--- src/components/HistoryFinanceCard/style.ts
+++ src/components/HistoryFinanceCard/style.ts
@@ -29,6 +29,10 @@ export const Container = styled.li`
     flex-direction: column;
     justify-content: space-between;
   }
+  > div span {
+    font-size: 20px;
+    font-weight: 500;
+  }
 `;
 export const Tag = styled.div<ITagProps>`
   position: absolute;
