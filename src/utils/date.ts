c156b231730c0223a266b59068f5c4e518935be5
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2022-04-12 23:43:35 -0300

    feat: created new filter of date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -5,3 +5,28 @@ export const formatDate = (date: string): string => {
   });
   return formatter.format(dateFormatted);
 };
+
+export enum NameMonth {
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Agosto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro',
+}
+
+export const filterDate = (list: any, type: 'year' | 'month'): number[] => {
+  let unique: number[] = [];
+  list.forEach((item: any) => {
+    const date = new Date(item.date);
+    const period = type === 'year' ? date.getFullYear() : date.getMonth();
+    if (!unique.includes(period)) unique.push(period);
+  });
+  return unique.sort();
+};
